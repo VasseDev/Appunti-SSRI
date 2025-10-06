98aa0d36926328ad4f8ce8b68efc1858cce49180
Pietro Vassena <pietro@vassena.dev>
2025-10-06 18:57:13 +0200

    Esercizio 5

---
+++ Primo/Programmazione/Esercizi/es05.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int main(int argc, char *argv[]) {
+    printf("CHAR_MAX: %d\n", CHAR_MAX);
+    printf("CHAR_MIN: %d\n", CHAR_MIN);
+    printf("INT_MAX: %d\n", INT_MAX);
+    printf("INT_MIN: %d\n", INT_MIN);
+    return 0;
+}
